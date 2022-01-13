2b10172c8e0639c304034308f435d2d061952830
Nikhil Ezhuvankandath <nikhilev@gitl.com>
2022-01-13 17:19:03 +0530

    Backend added

--- src/App.js
+++ src/App.js
@@ -13,41 +13,57 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { loadEmployees } from './actions/action-creators';
 
+//Amplify related packages
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsEXports from './aws-exports';
+
+Amplify.configure(awsEXports);
+
 function App({ loadEmployees }) {
 
   loadEmployees();
 
   return (
-    <Router>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">{process.env.REACT_APP_EMPLOYEE_APPLICATION_NAME}</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div id="pageContainer">
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/contact" element={<Contact />}></Route>
-          <Route exact path="/employees/loc/:locId/ecode/:ecode" element={<EmployeeDetail />}></Route>
-          <Route exact path="/employees/create" element={<EmployeeForm />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <Authenticator loginMechanisms={['username']}>
+      {({ signOut, user }) => (
+        <Router>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">{process.env.REACT_APP_EMPLOYEE_APPLICATION_NAME}</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav.Link href="#" onClick={signOut}>Signout</Nav.Link>
+                <Nav.Link href="#">Welcome {user.username}</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+          <div id="pageContainer">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+              <Route exact path="/employees/loc/:locId/ecode/:ecode" element={<EmployeeDetail />}></Route>
+              <Route exact path="/employees/create" element={<EmployeeForm />}></Route>
+            </Routes>
+          </div>
+        </Router>
+      )}
+
+    </Authenticator>
+
   );
 }
 
-function mapDispatchToProps(dispatch){
-  let actionMap={
+function mapDispatchToProps(dispatch) {
+  let actionMap = {
     loadEmployees
   }
 
-  return bindActionCreators(actionMap,dispatch)
+  return bindActionCreators(actionMap, dispatch)
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
